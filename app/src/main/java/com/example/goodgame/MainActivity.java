b120a75752c4d19fd82dc6d5bc7ad85b30254ae2
pineapple <xtian4@student.unimelb.edu.au>
2019-09-17 12:27:51 +1000

    second edition

--- app/src/main/java/com/example/goodgame/MainActivity.java
+++ app/src/main/java/com/example/goodgame/MainActivity.java
@@ -1,16 +1,14 @@
 package com.example.goodgame;
 
-<<<<<<< Updated upstream
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-=======
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
->>>>>>> Stashed changes
+
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -26,8 +24,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-<<<<<<< Updated upstream
-=======
 
->>>>>>> Stashed changes
 }
