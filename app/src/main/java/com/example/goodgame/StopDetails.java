b822200e3e069a42fe656ab5f4f01d19448719f8
Nicky Huang <xianchenh@student.unimelb.edu.au>
2019-09-17 13:05:47 +1000

    third

--- app/src/main/java/com/example/goodgame/StopDetails.java
+++ app/src/main/java/com/example/goodgame/StopDetails.java
@@ -2,8 +2,8 @@ package com.example.goodgame;
 
 
 import com.google.android.gms.maps.model.LatLng;
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class StopDetails {
@@ -23,8 +23,10 @@ public class StopDetails {
 
     }
 
-    //final FirebaseDatabase database = FirebaseDatabase.getInstance();
-    //DatabaseReference ref = database.getReference("server/saving-data/fireblog/posts");
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    {
+        DatabaseReference ref = database.getReference("server/saving-data/fireblog/posts");
+    }
 
     public LatLng getCoordinate() {
         this.coordinate= new LatLng(-37.798439, 144.964270);
