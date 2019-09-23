5cd2a499a15e5b7f89a6aecc767b325c0dd48f34
pineapple <xtian4@student.unimelb.edu.au>
2019-09-23 20:05:00 +1000

    gps

--- app/src/main/java/com/example/goodgame/ListActivity.java
+++ app/src/main/java/com/example/goodgame/ListActivity.java
@@ -1,8 +1,14 @@
 package com.example.goodgame;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
+import android.provider.Settings;
 import android.view.View;
 
 import android.widget.Button;
@@ -10,19 +16,16 @@ import android.widget.TextView;
 
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.google.gson.JsonObject;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -35,19 +38,26 @@ public class ListActivity extends AppCompatActivity {
     private static final String name = "Name";
     private static final String description = "Description";
     private static final Boolean isFreeZone = false;
-    FirebaseFirestore db;
-    TextView textDisplay;
-    String TAG = "ListActivity";
-    ArrayList<JSONObject> stopArray= new ArrayList<>();
+    private FirebaseFirestore db;
+    private TextView textDisplay;
+    private ArrayList<JSONObject> stopArray;
 
+    private TextView textView;
+    private LocationListener locationListener;
+    private LocationManager locationManager;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         db = FirebaseFirestore.getInstance();
+        stopArray = new ArrayList<>();
+        ReadSingleContact();
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-
+        //add floating button
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,6 +68,7 @@ public class ListActivity extends AppCompatActivity {
             }
         });
 
+        //add the top two buttons
         Button btnMap = (Button) findViewById(R.id.btnMap);
         btnMap.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,21 +87,69 @@ public class ListActivity extends AppCompatActivity {
             }
         });
 
+        //add text read from the database
         textDisplay = findViewById(R.id.textView);
         textDisplay.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                   Intent intent4 = new Intent(ListActivity.this, DetailActivity.class);
-                   startActivity(intent4);
+                Intent intent4 = new Intent(ListActivity.this, DetailActivity.class);
+                startActivity(intent4);
             }
         });
+        textView = (TextView)findViewById(R.id.textView7);
+        //using gps
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            //Whenever the location is updated, the last method checks if the gps is turned off.
+            @Override
+            public void onLocationChanged(Location location) {
+                textView.append("\n"+location.getLatitude()+location.getLongitude());
+            }
 
-        ReadSingleContact();
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
 
+            }
 
+            @Override
+            public void onProviderEnabled(String s) {
 
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                Intent intent5 = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent5);
+            }
+        };
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.INTERNET
+                }, 10);
+                return;
+            }
+        } else {
+            locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+
+        }
     }
+
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode){
+            case 10:
+                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+                return;
+        }
+    }
+
+
+
+
 
     private void ReadSingleContact() {
         CollectionReference collectionReference = db.collection("stopInfo");
@@ -113,6 +172,7 @@ public class ListActivity extends AppCompatActivity {
                             stop.put("name", document.get("name"));
                             stopInfo.put("id",document.getId());
                             stopInfo.put("info", stop);
+                            stopArray.add(stopInfo);
 
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -122,8 +182,6 @@ public class ListActivity extends AppCompatActivity {
                 }
             }
         });
-
     }
-
 
 }
