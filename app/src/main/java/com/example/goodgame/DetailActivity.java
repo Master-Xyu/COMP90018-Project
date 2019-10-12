e3fc20a17278c89d45645f643a9f984d8383c3f3
Agatha_Ma <max995@126.com>
2019-10-13 02:37:04 +1100

    POST TEST FINISH
    but wrong when uploading images, and have difficulties in connecting with map part

--- app/src/main/java/com/example/goodgame/DetailActivity.java
+++ app/src/main/java/com/example/goodgame/DetailActivity.java
@@ -1,12 +1,32 @@
 package com.example.goodgame;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private Button postBtn;
+
     protected void onCreat(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.details);
+        postBtn = (Button)findViewById(R.id.action_add_post);
+        postBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPostActivity();
+            }
+        });
     }
+
+    private void openPostActivity() {
+        Intent intent =new Intent(this,AddPostActivity.class);
+        startActivity(intent);
+
+    }
+
 }
