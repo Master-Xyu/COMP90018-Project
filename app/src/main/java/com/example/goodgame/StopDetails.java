75df7ad263e7ca21f0706fbd6769448d87e39172
Agatha_Ma <max995@126.com>
2019-10-12 22:25:31 +1100

    change dependencies and add post but no test

--- app/src/main/java/com/example/goodgame/StopDetails.java
+++ app/src/main/java/com/example/goodgame/StopDetails.java
@@ -1,7 +1,12 @@
 package com.example.goodgame;
 
 
+import android.content.Intent;
+import android.view.MenuItem;
+
 import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.GeoPoint;
 
 
@@ -11,6 +16,7 @@ public class StopDetails {
     private String name, description;
     private Boolean isFreeZone;
     private LatLng position;
+    FirebaseAuth firebaseAuth;
 
     public StopDetails() {
 
@@ -37,5 +43,28 @@ public class StopDetails {
 
     public LatLng getPosition() {
         return position;
+    }
+
+    /*handle menu item clicks*/
+    public  boolean onOptionsItemSelected(MenuItem item){
+        int id =item.getItemId();
+        if(id==R.id.action_add_post) {
+        startActivity(new Intent(getActivity(),AddPostActivity.class))
+        }
+
+    }
+
+    public  void checkUserStatus(){
+        //get current user
+        FirebaseUser user =firebaseAuth.getCurrentUser();
+        if(user!=null){
+            //???
+            //
+            //
+        }
+        else{
+            startActivity(new Intent(getActivity(),MainActivity.class));
+            getActivity().finish();
+        }
     }
 }
